f2d2770339e4a4189ec285411e447b64d41d3710
Robert <robertnicolaesolca@gmail.com>
2022-11-09 18:45:12 +0200

    added crack

--- src/cracker.py
+++ src/cracker.py
@@ -1,12 +1,26 @@
 from utils import get_lines_from_file
+from crypto import get_sha256
 
 class Cracker():
-    def __init__(self, wordlist_path, hashes_path, hash_function) -> None:
+    def __init__(self, wordlist_path, hash_function) -> None:
         self.wordlist_path = wordlist_path
-        self.hashes_path = hashes_path
         self.hash_function = hash_function
-        self.init_files()
-
-    def init_files(self):
         self.word_list = get_lines_from_file(self.wordlist_path)
-        self.hashes_path = get_lines_from_file(self.hashes_path)
+        self.rainbow_table = self.get_rainbow_table()
+
+    def get_rainbow_table(self):
+        rainbow_table = {}
+        for word in self.word_list:
+            hash = get_sha256(word)
+            rainbow_table[hash] = word
+        return rainbow_table
+
+    def crack(self, hashes_path):
+        hash_list = get_lines_from_file(hashes_path)
+        result_map = {}
+        for hash in hash_list:
+            if hash in self.rainbow_table:
+                result_map[hash] = self.rainbow_table[hash]
+            else:
+                result_map[hash] = None
+        
